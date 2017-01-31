e78dbf11f9e6b12ec195d1e8c0908a99da039a86
kashifali554 <kashifali874@gmail.com>
2017-01-30 16:09:56 -0800

    updated js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,20 @@
-console.log("I am connected")
 
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king"
 var cardFour = "king"
+
+if (cardOne === cardTwo){ 
+	alert("You found a match"); 
+} else { 
+	alert("Sorry, try again");
+}
+
+//We'll work with it to finalize our memory 
+//game during the next unit.
+
+var card = getElementByid('game-board')
+
+for(var i =0; var < 0; var i++){
+	
+}
